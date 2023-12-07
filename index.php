3b48a818a216749cef9be630201fada1b3ae9cb2
Alejandro Barraza <abarraza.pk@gmail.com>
2023-12-07 11:57:28 -0300

    Poniendo el código en index.php test

--- index.php
+++ index.php
@@ -638,9 +638,39 @@
                 <textarea class="form-control" name="mensaje" rows="5" placeholder="Mensaje" required></textarea>
               </div>
               <div class="my-3">
-                <?php
-                  include 'forms/contact.php';
-                ?>
+<?php
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
+
+  // Replace contact@example.com with your real receiving email address
+
+  $direccion_email_receptora = 'abarraza.pk@gmail.com';
+
+  if (isset($_POST['enviar'])) {
+    if (!empty($_POST['nombre']) and !empty($_POST['email']) and !empty($_POST['asunto']) and !empty($_POST['mensaje'])) { 
+      $nombre = $_POST['nombre'];
+      $email = $_POST['email'];
+      $asunto = $_POST['asunto'];
+      $mensaje = $_POST['mensaje'];
+      
+      $encabezado = "From: " . $email .  "\r\n";
+      $encabezado .= "Reply-To: " . $direccion_email_receptora . "\r\n";
+      $encabezado .= "X-Mailer: PHP/" . phpversion() . "\r\n";
+
+      
+      $mail = @mail($direccion_email_receptora, $asunto, $mensaje, $encabezado);
+      if ($mail) {
+        echo('<div class="sent-message d-block">Mensaje Enviado</div>');
+      }
+    }
+  }
+
+
+?>
                 <div class="loading">Cargando...</div>
                 <div class="error-message"></div>
               </div>
